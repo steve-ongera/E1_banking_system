c5ab262d8bef3bc7af6ec081a16d1da39c52b373
steve-ongera <gadafisteve001@gmail.com>
2026-06-14 23:16:52 +0300

    made updates

---
+++ django_backend/api/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'account_number', 'balance', 'phone']
+        read_only_fields = ['account_number', 'balance']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'phone']
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        user.phone = validated_data.get('phone', '')
+        user.account_number = f"ACC{user.id:08d}"
+        user.save()
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+    
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials")
+
+class BalanceSerializer(serializers.Serializer):
+    balance = serializers.DecimalField(max_digits=12, decimal_places=2)
