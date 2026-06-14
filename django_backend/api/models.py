62d0fe09abdbfe9018d4b6251d2bff436a5a2d3b
steve-ongera <gadafisteve001@gmail.com>
2026-06-14 23:16:03 +0300

    made updates

--- django_backend/api/models.py
+++ django_backend/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class User(AbstractUser):
+    account_number = models.CharField(max_length=20, unique=True)
+    balance = models.DecimalField(max_digits=12, decimal_places=2, default=0.00, validators=[MinValueValidator(0)])
+    phone = models.CharField(max_length=15, blank=True)
+    
+    def __str__(self):
+        return f"{self.username} - {self.account_number}"
